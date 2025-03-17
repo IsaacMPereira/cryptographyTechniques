f456722d01c5475991beda8fd2a0ef28b9d5e465
IsaacMPereira <isaacp.menezesp@gmail.com>
2025-03-17 20:14:05 -0300

    Advancing in this algorithm

--- shiftedAlphabet.cpp
+++ shiftedAlphabet.cpp
@@ -5,15 +5,42 @@
 using namespace std;
 
 //Assignments for all the functions
+void crypting();
 string upperCase(string s);
 string generatingAlphabet();
 string shiftedAlphabet(int shift, string alfa);
+void relatting(string text, string alfa);
 
 int main(){
+	int opt;
+
+	cout << "Options:\n1 - Crypt text\n2 - Decrypt text\n3 - Check wether is a cipher text crypted with this method\nChoose: ";
+	cin >> opt;
+	getchar();
+	fflush(stdin);
+	switch(opt){
+		case 1:
+			crypting();
+			break;
+		case 2:
+			decrypting();
+			break;
+		case 3:
+			cout << "@" << endl;
+			break;
+		default:
+			cout << "Invalid option!" << endl;
+	}	
+
+	cout << endl;
+	return 0;
+}
+
+void crypting(){
 	string plainText, cipherText;
 	int shift;
 	vector<int> relations;
-	
+
 	cout << "Plain text: ";
 	getline(cin, plainText);
 	string plainText_lc = upperCase(plainText); //Putting in lower case
@@ -22,40 +49,54 @@ int main(){
 	cin >> shift;
 
 	if(shift > 26){
-	    cout << "Shift must be smaller then 26" << endl;
-	    exit(1);
+		cout << "Shift must be smaller then 26" << endl;
+		exit(1);
 	}else if(shift <= 0){
-	    cout << "Shift must be bigger then 0" << endl;
-	    exit(1);
+		cout << "Shift must be bigger then 0" << endl;
+		exit(1);
 	}
 
 	string alfa = generatingAlphabet();	
-	
+
 	string alfaShifted = shiftedAlphabet(shift, alfa);	
-	 
-	 //Relating the cipher text characteres positions with it's corresponding positions in the alphabet
-	 for(size_t i=0; i<plainText_lc.size(); i++){
-	     for(size_t j=0; j<alfa.size(); j++){
-	          if(plainText_lc.at(i) == alfa.at(j))
-	              relations.push_back(j);
-	              
-	      }
-	 }
-	 
-	 cout << endl << "Cipher text: ";
-	 for(auto e : relations)
-	     cout << alfaShifted.at(e);
-	     
-	 /*cout << endl;
-	 for(auto e : alfa)
-	     cout << e << " ";
-	     
-	 cout << endl;
-	 for(auto e : alfaShifted)
-	     cout << e << " ";*/
-	 
-	 cout << endl;
-	 return 0;
+
+	relations = relatting(plainText_lc, alfa);
+
+	cout << endl << "Cipher text: ";
+	for(auto e : relations)
+		cout << alfaShifted.at(e);
+
+	cout << endl;
+	for(auto e : alfa)
+		cout << e << " ";
+
+	cout << endl;
+	for(auto e : alfaShifted)
+		cout << e << " ";
+}
+
+void decrypting(){
+	string cipherText, cipherText_lc;
+	int shift;
+	vector<int>relations;
+
+	cout << "Shift: ";
+	cin >> shift;
+
+	string alfa = generatingAlphabet();
+
+	string alfaShifted = shiftedAlphabet(alfa, shift);  
+
+	cout << "Cipher text: ";
+	cin >> cipherText;
+	
+	cipherText_lc = upperCase(cipherText);
+
+	relations = relatting(cipherText_lc, alfa);	
+
+	cout << "Plain text: ";
+	for(auto e : relations)
+		cout << alfa.at(e);	
 }
 
 string upperCase(string s){
@@ -78,7 +119,7 @@ string generatingAlphabet(){
 	string alfa;
 
 	for(int i=begin; i<='z'; i++){
-	    alfa.push_back(i);
+		alfa.push_back(i);
 	}
 
 	return alfa;
@@ -87,16 +128,34 @@ string generatingAlphabet(){
 string shiftedAlphabet(int shift, string alfa){
 	string alfaShifted;
 
+	//shift = 26 - shift;
+
 	int controller = 0;
 	while(controller != 26){
-	     if(shift == 26){
-	         shift = 0;
-	     }
-	     alfaShifted.push_back(alfa.at(shift));
-	    
-	     shift++;
-	     controller++;
-	 }
+		if(shift == 26){
+			shift = 0;
+		}
+		alfaShifted.push_back(alfa.at(shift));
+
+		shift++;
+		controller++;
+	}
 
 	return alfaShifted;
 }
+
+//Relatting the text characteres positions with it's corresponding positions in the alphabet
+void relatting(string text, string alfa){
+	vector<int>relations;
+
+	for(size_t i=0; i<text.size(); i++){
+		for(size_t j=0; j<alfa.size(); j++){
+			if(text.at(i) == alfa.at(j))
+				relations.push_back(j);
+
+		}
+	}
+
+	return relations;
+}
+
